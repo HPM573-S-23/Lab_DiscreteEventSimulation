7ca548bb7b2d774e73f4dbc2d30650692b2120ac
Reza Yaesoubi <reza.yaesoubi@yale.edu>
2020-02-24 10:41:14 -0500

    warm-up

--- ModelOutputs.py
+++ ModelOutputs.py
@@ -44,8 +44,7 @@ class SimOutputs:
         """
 
         # increment the number of patients arrived
-        if self.simCal.time > self.warmUpPeriod:
-            self.nPatientsArrived += 1
+        # self.nPatientsArrived += 1
 
         # update the sample path of patients in the system
         self.nPatientInSystem.record_increment(time=self.simCal.time, increment=+1)
@@ -86,10 +85,9 @@ class SimOutputs:
         self.nPatientInSystem.record_increment(time=self.simCal.time, increment=-1)
         self.nExamRoomBusy.record_increment(time=self.simCal.time, increment=-1)
 
-        if self.simCal.time > self.warmUpPeriod:
-            self.nPatientsServed += 1
-            self.patientTimeInWaitingRoom.append(time_waiting)
-            self.patientTimeInSystem.append(time_in_system)
+        self.nPatientsServed += 1
+        self.patientTimeInWaitingRoom.append(time_waiting)
+        self.patientTimeInSystem.append(time_in_system)
 
         # build the patient summary
         if self.traceOn:

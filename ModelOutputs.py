93e304b72b1e34bd9f6c514206301cd1b90245e4
Reza Yaesoubi <reza.yaesoubi@yale.edu>
2020-02-23 22:39:29 -0500

    collecting stats

--- ModelOutputs.py
+++ ModelOutputs.py
@@ -16,8 +16,6 @@ class SimOutputs:
         self.patientTimeInWaitingRoom = []  # observations on patients time in the waiting room
 
         # sample path for the patients waiting
-        self.nPatientsWaiting = Path.PrevalenceSamplePath(
-            name='Number of patients waiting', initial_size=0)
 
         # sample path for the patients in system
         self.nPatientInSystem = Path.PrevalenceSamplePath(
@@ -32,65 +30,34 @@ class SimOutputs:
         :param patient: the patient who just arrived
         """
 
-        # increment the number of patients arrived
-        self.nPatientsArrived += 1
-
-        # update the sample path of patients in the system
-        self.nPatientInSystem.record_increment(time=self.simCal.time, increment=+1)
-
-        # store arrival time of this patient
-        patient.tArrived = self.simCal.time
 
     def collect_patient_joining_waiting_room(self, patient):
         """ collects statistics when a patient joins the waiting room
         :param patient: the patient who is joining the waiting room
         """
 
-        # store the time this patient joined the waiting room
-        patient.tJoinedWaitingRoom = self.simCal.time
-
-        # update the sample path of patients waiting
-        self.nPatientsWaiting.record_increment(time=self.simCal.time, increment=1)
 
     def collect_patient_leaving_waiting_room(self, patient):
         """ collects statistics when a patient leave the waiting room
         :param patient: the patient who is leave the waiting room
         """
 
-        # store the time this patient leaves the waiting room
-        patient.tLeftWaitingRoom = self.simCal.time
-
-        # update the sample path
-        self.nPatientsWaiting.record_increment(time=self.simCal.time, increment=-1)
 
     def collect_patient_departure(self, patient):
         """ collects statistics for a departing patient
         :param patient: the departing patient
         """
 
-        time_waiting = patient.tLeftWaitingRoom-patient.tJoinedWaitingRoom
-        time_in_system = self.simCal.time-patient.tArrived
-
-        self.nPatientsServed += 1
-        self.nPatientInSystem.record_increment(time=self.simCal.time, increment=-1)
-        self.nExamRoomBusy.record_increment(time=self.simCal.time, increment=-1)
-        self.patientTimeInWaitingRoom.append(time_waiting)
-        self.patientTimeInSystem.append(time_in_system)
 
     def collect_patient_starting_exam(self):
         """ collects statistics for a patient who just started the exam """
 
-        self.nExamRoomBusy.record_increment(time=self.simCal.time, increment=+1)
 
     def collect_end_of_simulation(self):
         """
         collects the performance statistics at the end of the simulation
         """
 
-        # update sample paths
-        self.nPatientsWaiting.close(time=self.simCal.time)
-        self.nPatientInSystem.close(time=self.simCal.time)
-        self.nExamRoomBusy.close(time=self.simCal.time)
 
     def get_ave_patient_time_in_system(self):
         """
